8dcde1eb7237162e3c84d4dd3c2a14d5c3beb511
riojapri <80973244+riojapri@users.noreply.github.com>
2022-03-28 13:54:53 +0700

    Add files via upload

---
+++ student/src/main/java/id/ac/uin/student/student/entity/Student.java
@@ -0,0 +1,40 @@
+package id.ac.uin.student.student.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty(message = "First name is required")
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @NotEmpty(message = "Email is required")
+    @Column(nullable = true, name = "email", unique = true)
+    private String email;
+
+    @NotEmpty(message = "Phone number is required")
+    @Column(nullable = true, unique = true)
+    private String phone;
+
+    @NotEmpty(message = "Jurusan is required")
+    @Column(nullable = false, unique = true)
+    private String jurusan;
+
+}
